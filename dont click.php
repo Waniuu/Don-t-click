8f178a3e3f623071b15814e49b11fe7917d4d5eb
Waniuu <88043852+Waniuu@users.noreply.github.com>
2022-09-19 18:07:26 +0200

    Add files via upload

---
+++ dont click.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="PL-pl">
+    <head>
+        <meta charset="UTF-8">
+        <title>Why are u racist</title>
+        <link rel="stylesheet" href="styl2269.css">
+    </head>
+    <body>
+        <div class="header">
+            <H1>Why are u Homophobic?</H1>
+        </div>
+        <div class="main">
+            <br>
+            <br>
+            <a href=""><img class="btn" src="obrazeczek.png" alt="Logo lotniska" ></a>
+                <script>
+                    var btn = document.querySelector(".btn");
+                    var position;
+                    btn.addEventListener("mouseover", function () {
+                        position ? (position = 0) : (position = 727);
+                        btn.style.transform =  `translate(${position}px,0px)`;
+                        btn.style.transition = "all 1.0s ease";
+                    });
+                </script>
+        </div>
+        <div class="main2">
+            <br>
+            <br>
+            <br>
+            <a href=""><img class="btn2" src="no.png" alt="Logo lotniska" ></a>
+            <a href="noob.jpg"></a>
+        </div>
